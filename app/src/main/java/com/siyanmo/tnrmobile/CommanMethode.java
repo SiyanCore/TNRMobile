997400e3b7016a9d8b31f650360e3e156db25a62
Lakmal1991 <sgulakma@gmail.com>
2016-02-17 15:22:47 +0530

    //Lakmal

--- app/src/main/java/com/siyanmo/tnrmobile/CommanMethode.java
+++ app/src/main/java/com/siyanmo/tnrmobile/CommanMethode.java
@@ -5,9 +5,11 @@ package com.siyanmo.tnrmobile;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
-
-
+import android.util.Base64;
+import android.view.View;
+import android.widget.EditText;
 import com.google.gson.Gson;
+import com.siyanmo.tnrmobile.DomainObjects.User;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,44 +18,45 @@ import org.json.JSONObject;
  */
 public final class CommanMethode {
 
-
-    public static JSONObject ObjectToJsonString (Object myObj){
+    public static JSONObject ObjectToJsonString(Object myObj) {
         Gson gson = new Gson();
         String json = gson.toJson(myObj);
 
         JSONObject reqBody = null;
-       try {
-          reqBody = new JSONObject (json);
-       } catch (JSONException e) {
-           e.printStackTrace();
+        try {
+            reqBody = new JSONObject(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
         return reqBody;
     }
 
-    public static <T> T JsonToBoject (JSONObject json,Class<T> obj){
-        T newObject =null ;
+    public static <T> T JsonToBoject(JSONObject json, Class<T> obj) {
+        T newObject = null;
         Gson gson = new Gson();
-       //newObject = gson.fromJson(json, obj.class);
+        //newObject = gson.fromJson(json, obj.class);
         return newObject;
     }
-    //Lakmal//
-    //Save User Name And Password
-    //class  SaveUserNamePassword extends AppCompatActivity {
-        //public void SaveUserNamePassword(String Uname, String Pword) {
-           // String UserName = Uname;
-           // String PassWord = Pword;
 
-           // SharedPreferences sharedPreferences = getSharedPreferences("TNRMobile_Login_User_Information", Context.MODE_PRIVATE);
-            //SharedPreferences.Editor editor = sharedPreferences.edit();
-           // editor.putString("Uname", UserName);
-            //editor.putString("Password", PassWord);
-            //editor.commit();
-        }
-
+    public static void LogUserDetails(SharedPreferences sharedPreferences, User users) {
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putString("Uname",users.getLoginName());
+        editor.putString("Password",users.getPassword());
+        editor.commit();
+    }
+    //Password encrypt
+   // public static String encrypt(String input) {
+        // Simple encryption, not very strong!
+      //  return Base64.encodeToString(input.getBytes(), Base64.DEFAULT);
    // }
+    //Password decrypt--
+  //  public static String decrypt(String input) {
+     //   return new String(Base64.decode(input, Base64.DEFAULT));
+    }
 
-      //  }
-//
+
+
+
 
 
 
