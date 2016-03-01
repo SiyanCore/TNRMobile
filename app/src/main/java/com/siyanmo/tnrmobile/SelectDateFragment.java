8d72a0baca9ee9243afffeda2e39fc155a5dd722
Lovindu <lkalana89@gmail.com>
2016-03-01 13:42:08 +0530

    lk

---
+++ app/src/main/java/com/siyanmo/tnrmobile/SelectDateFragment.java
@@ -0,0 +1,33 @@
+package com.siyanmo.tnrmobile;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by Administrator on 2016-02-29.
+ */
+public class SelectDateFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int yy = calendar.get(Calendar.YEAR);
+        int mm = calendar.get(Calendar.MONTH);
+        int dd = calendar.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(), this, yy, mm, dd);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        populateSetDate(year, monthOfYear+1, dayOfMonth);
+    }
+
+    public void populateSetDate(int year, int month, int day) {
+       // dob.setText(month + "/" + day +"/"+year);
+    }
+
+}
